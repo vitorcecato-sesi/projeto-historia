ad518b67e8b00542483df45493eeff01c3e22186
Lucas C <lucascasagrandesesi@gmail.com>
2025-05-15 15:50:01 -0300

    Término do esqueleto da page e começo do css (fazer carousel)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import FascismoItaliano from './pages/FascismoItaliano'
 import Home from './pages/Home'
 
 
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-    <Home/>
+    <FascismoItaliano/>
     </>
   )
 }
---
+++ src/pages/FascismoItaliano.jsx
@@ -0,0 +1,117 @@
+import "./style/FascismoItaliano.css"
+import Navbar from "../components/Navbar"
+import { Link } from "react-router-dom"
+
+function FascismoItaliano(){
+    return(
+<>
+<Navbar/>
+<section className="box-geral">
+<h1 className="tituloF">CONHEÇA MAIS SOBRE O <span className="verdeTexto">FASCISMO</span> <span className="vermelhoTexto">ITALIANO...</span></h1>
+<section className="box-infow">
+    <h2>INFORMAÇÕES WIKIPÉDIA</h2>
+    <p>bla bla bla</p>
+</section>
+
+<section className="box-info">
+    <section className="box-coluna">
+    <h2>O QUE É?</h2>
+    <p> O fascismo foi um regimento político totalitário liderado por Benito Mussolini, o movimento surgiu na itália em 1910 e foi oficializado como partido político em 1922, governando a itália 
+até 1943. O fascismo dominou o país inteiro (educação, economia, religião e política).
+ </p>
+    </section>
+    <img src="https://cartasabertas.com.br/wp-content/uploads/2022/09/capa-nazismo-bandeira.jpg"/>
+</section>
+
+<section className="box-info">
+    <img src="https://static.historiadomundo.com.br/2024/09/benito-mussolini-lider-do-fascismo-italiano-entre-membros-dos-camisas-negras-especie-de-milicia-fascista.jpg"/>
+    <section className="box-coluna">
+    <h2>CONTEXTO HISTÓRICO</h2>
+    </section>
+    <p>Após a primeira guerra mundial, a Europa estava em caos, com uma grande crise econômica. A população se encontrava bastante desanimada.
+ São nessas condições que surgem alguns políticos e ideólogos, com discursos relacionados a resolver problemas complexos e  falar o que o povo mais queria ouvir, portanto, esse líder facista aproveitou o momento para apresentar um discurso parecido com o citado, sendo parecido com "Levante-se, orgulhe-se! A nação precisa de você, nós somos superiores".
+ Vale ressaltar que a Itália individualmente falando, também se encontrava em uma crise econômica, com sua moeda desvalorizada, aumento da inflação, a taxa de desemprego aumentando, diversas revoltas, greves e invasões de terra.
+ Em meio a tudo isso, os partidos de esquerda ganharam força, fato esse que incomodou os latifundiários e burgueses.
+ Todos esses fatores "justificam" a força e popularidade do fascismo italiano na época.
+</p>
+</section>
+
+<section className="box-info">
+     <section className="box-coluna">
+    <h2>GOVERNO FASCISTA</h2>
+    <p>Depois da vitória nas eleições de maneira fraudulenta, o Partido Fascista se consolida no poder. Mussolini promulgou as "Leis Fascistíssimas" que deixaram claro quem mandava no país.
+ Estas leis determinaram que o Partido Nacional Fascista era o único partido existente e o Grande Conselho Fascista, presidido por Mussolini, era o órgão supremo do Estado. Ainda determinava que as organizações civis deveriam ser controladas pela polícia e os sindicatos fascistas eram os únicos realmente reconhecidos.
+ Os funcionários públicos deveriam jurar fidelidade ao regime e aqueles
+que recusassem eram demitidos.
+ Em 1927, foi apresentada uma carta (Carta del Lavoro) que instruiu como eram para ser conduzidas as relações de trabalho no país. A Carta garantia a propriedade privada e determinava que a organização dos sindicatos deveriam ser feitas pelo Estado.
+ Em 1930, o fascismo incorpora o discurso de expansão territorial, ao declarar guerra à Etiópia. O conflito tinha como objetivo exaltar a "raça italiana", seus princípios e virtudes. Igualmente é época quando Mussolini se aproxima de Adolf Hitler, tendo como resultado
+a promulgação das leis antissemitas onde os judeus italianos perderam seus direitos civis.
+</p>
+    </section>
+    <img src="https://www.politize.com.br/wp-content/uploads/2024/07/fascismo-entenda-o-conceito.jpg"/>
+</section>
+
+<section className="box-info">
+    <img src="https://medias.revistaoeste.com/wp-content/uploads/2022/08/fascismo-e-de-esquerda-2.jpg"/>
+     <section className="box-coluna">
+    <h2>FIM DO GOVERNO</h2>
+    <p>O governo de Mussolini chega ao seu final em 1943, em um momento de sérias derrotas da Itália durante a Segunda Guerra Mundial. Assustado, Mussolini é levado pelos alemães para o norte e quando tenta escapar para a Alemanha é capturado e morto pelos partisans.
+</p>
+    </section>
+</section>
+
+<section className="box-info">
+    <section className="box-coluna">
+    <h2>CARACTERÍSTICAS</h2>
+    <p>1. Sistema unipartidário ou monopartidário, no qual apenas o próprio partido fascista tinha direito à atuação no sistema político nacional;
+
+2. Culto ao chefe/líder como forma de colocá-lo como a única pessoa capaz de guiar a nação ao seu destino;
+
+3. Desprezo pelos valores liberais, nos quais estão inclusas as liberdades individuais e a democracia representativa;
+
+4. Desprezo por valores coletivistas, como o socialismo, comunismo e anarquismo;
+
+5. Desejo de expansão imperialista baseada na ideia de domínio de povos mais fracos;
+
+6. Vitimização de determinados grupos da sociedade ou de um povo com o objetivo de iniciar uma perseguição contra aqueles que eram vistos como “inimigos do povo”;
+
+7. Uso da retórica contra os métodos políticos tradicionais afirmando que estes eram incapazes de combater as crises e de levar a nação à prosperidade;"
+
+8. Exaltação dos “valores tradicionais” em detrimento de valores considerados “modernos;
+
+9. Mobilização das massas;
+
+10. Controle total do Estado fascista sobre assuntos relacionados à economia, política e cultura."
+</p>
+    </section>
+    <img src="https://static.mundoeducacao.uol.com.br/mundoeducacao/2020/02/benito-mussolini-me.jpg"/>
+</section>
+
+<section className="box-info">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Benito_Mussolini_colored.jpg/500px-Benito_Mussolini_colored.jpg"/>
+     <section className="box-coluna">
+    <h2>BENITO MUSSOLINI</h2>
+    <p>Benito Mussolini, o líder do movimento italiano, juntou diversos seguidores e conseguiu o poder do governo.
+ Nascido em 29 de julho de 1883, em uma cidade pequena localizada no sul da Itália, filho de pai ferreiro e mãe professora, cresceu com as histórias de nacionalismo e heroísmo político de seu pai socialista. Ainda jovem mudou-se para a Suíça e tornou-se um socialista
+declarado, quando voltou para Itália, se estabeleceu como um jornalista socialista, porém futuramente Mussolini mudou sua ideologia política e criou um partido de extrema direita, em 1919.
+</p>
+    </section>
+</section>
+
+<section className="box-filmes">
+    <h1>HORA DO FILME...</h1>
+    <section>
+{/* tag do youtube */}
+<p></p>
+    </section>
+     <section>
+{/* tag do youtube */}
+<p></p>
+    </section>
+</section>
+</section>
+</>
+    )
+}
+
+export default FascismoItaliano
