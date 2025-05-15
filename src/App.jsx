f3873394cafb8b665f7d7e8b2a402318eb956ad0
Vitor Cecato <vitor.cecatosesi@gmail.com>
2025-05-15 10:10:48 -0300

    Inicio da Página

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import './App.css'
+// Hooks
+  import { BrowserRouter, Routes, Route } from 'react-router-dom'
+//.
+
+// Estilização
+  import './App.css'
+//.
+
+// Páginas
+  import RevolucaoRussa from './pages/RevolucaoRussa'
+//.
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<RevolucaoRussa />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/RevolucaoRussa.jsx
@@ -0,0 +1,31 @@
+// Estilização
+    import './style/RevolucaoRussa.css'
+//.
+
+// Componentes
+
+//.
+
+// Imagens
+
+//.
+
+function RevolucaoRussa() {
+
+
+    return(
+        <>
+            <section className='bodyRR'>
+                <section className='bannerRR'>
+                    <h1 className='bannerTextRR'>CONHEÇA MAIS SOBRE A <br /><span id='blueRR'>REVOLUÇÃO RUSSA</span></h1>
+                </section>
+                <section className='wikiRR'>
+                    <h1>Informações Wikipédia</h1>
+                    <p></p>
+                </section>
+            </section>
+        </>
+    )
+}
+
+export default RevolucaoRussa
