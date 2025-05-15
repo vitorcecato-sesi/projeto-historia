6dbab2dadfa79d319552427bba40cffa4acf01bf
Laura Betti <bettilaura0@gamail.com>
2025-05-15 12:09:00 -0300

    Página revolução de 1930

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import RevolucaoDe1930 from './pages/RevolucaoDe1930'
+import Home from "./pages/Home"
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/Revolução-de-1930' element={<RevolucaoDe1930/>}/>         
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
