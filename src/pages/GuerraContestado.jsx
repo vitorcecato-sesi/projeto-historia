82204809c802f950f588414bea978cddb12517d2
Extreme <npyetro26@gmail.com>
2025-05-21 08:37:20 -0300

    Arrumando css

--- src/pages/GuerraContestado.jsx
+++ src/pages/GuerraContestado.jsx
@@ -1,7 +1,6 @@
 
-import Navbar from "../components/Navbar";
 import './style/GuerraContestado.css'
-import Navbar from '../components/Navbar'
+import Navbar from "../components/Navbar";
 function GuerraContestado() {
     return (
         <div>
@@ -23,13 +22,21 @@ function GuerraContestado() {
             <section className='centro'>
             <section className='blocoInfo'>
             <h2>Resumo</h2>
-            <p>A Guerra do Contestado foi um conflito armado que ocorreu entre 1912 e 1916, na região do Contestado, que abrange partes dos estados de Santa Catarina e Paraná, no Brasil. O conflito envolveu a disputa entre os habitantes locais, conhecidos como "caboclos", e as forças do governo brasileiro, além de empresas madeireiras que exploravam a região. A guerra teve raízes em questões sociais, econômicas e políticas, incluindo a luta pela posse da terra e a resistência à exploração econômica.</p>
+            <p>Ocorreu em uma região entre Santa Catarina e Paraná (no sul do Brasil) entre 1912 e 1916.
+O conflito se desenvolveu com milhares de pessoas se deslocando para o local, com o objetivo de trabalharem na construção. Os camponeses, que moravam no local, foram expulsos de suas terras para a construção da estrada de ferro.
+Com a conclusão da ferrovia, os trabalhadores (que construíram a mesma) ficaram desempregados, provocando uma crise social.
+A população local do período, influenciada por líderes religiosos, como José Maria do Santo Agostinho, juntamente de monges messiânicos, formaram comunidades com os sertanejos empobrecidos.
+O Presidente da república, vendo esta movimentação, se preocupou e enviou várias tropas para a região que, mais tarde, foram derrotadas pela população.
+No meio tempo, José Maria foi assassinado, mas seus pensamentos continuaram em evidência e a guerra permaneceu.
+Após, o governo comprou a luta de forma grosseira e a tornou mais sangrenta (armamentos pesados e aviões de artilharia foram utilizados), matando os milhares de sertanejos e fazendo um acordo que estabelecesse os limites de cada região.
+
+</p>
             
             </section>
             <section className='blocoInfo'>
             <h2>Motivo</h2>
             <section className='blocoInfo2'>
-            <p>O principal motivo da Guerra do Contestado foi a disputa pela posse da terra e a exploração econômica da região. A chegada de empresas madeireiras, como a Companhia de Estrada de Ferro São Paulo-Rio Grande, levou à desapropriação de terras dos caboclos, que eram os habitantes locais. Além disso, havia uma forte influência religiosa na região, com a presença de líderes messiânicos que prometiam libertar o povo da opressão.</p>
+            <p>O estopim para a guerra foi a disputa das terras na região (sendo o nomeador da guerra; "Contestado" pela região ser contestada) e a construção de uma estrada de ferro pela empresa Brazil Railway</p>
                 <img id='imgGC' src='https://media.gazetadopovo.com.br/2016/10/9a43ec7da1daae2c8b5f7178aebe75e9-gpLarge.jpg' alt='Guerra do Contestado' />
             </section>
             
@@ -37,8 +44,15 @@ function GuerraContestado() {
              <section className='blocoInfo'>
             <h2>Desenvolvimento </h2>
             <section className='blocoInfo2'>
+                <section className='info'>
                 <img id='imgGC' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyBVlWEP-XnVV_NfwNHLt7OaI4TtjSBH7ZmA&s' alt='Guerra do Contestado' />
-            <p>A guerra começou em 1912, quando os caboclos se organizaram em grupos armados para resistir à exploração das terras e à presença das forças do governo. O conflito se intensificou em 1914, quando os caboclos, liderados por figuras messiânicas como José Maria, proclamaram a criação de um "Reino de Deus" na região. As forças do governo responderam com repressão violenta, resultando em batalhas sangrentas e massacres de civis.</p>               
+           <p>O conflito se desenvolveu com milhares de pessoas se deslocando para o local, com o objetivo de trabalharem na construção. Os camponeses, que moravam no local, foram expulsos de suas terras para a construção da estrada de ferro.
+Com a conclusão da ferrovia, os trabalhadores (que construíram a mesma) ficaram desempregados, provocando uma crise social.
+A população local do período, influenciada por líderes religiosos, como José Maria do Santo Agostinho, juntamente de monges messiânicos, formaram comunidades com os sertanejos empobrecidos.</p>
+           
+            </section>
+           
+
             </section>
             
             
@@ -47,7 +61,7 @@ function GuerraContestado() {
             <h2 >Fim da guerra</h2>
             <section className='blocoInfo2'>
              <img id='imgGC' src='https://media.gazetadopovo.com.br/2016/10/9a43ec7da1daae2c8b5f7178aebe75e9-gpLarge.jpg' alt='Guerra do Contestado' /> 
-            <p>A Guerra do Contestado terminou em 1916, após quatro anos de conflitos. O governo brasileiro conseguiu reprimir a resistência dos caboclos, mas o custo humano e social foi alto. Estima-se que milhares de pessoas tenham morrido durante o conflito, e a região do Contestado ficou marcada por traumas e divisões sociais. A guerra também teve um impacto significativo na história do Brasil, destacando as tensões entre o desenvolvimento econômico e os direitos das populações locais.</p>
+            <p>O Presidente da república, vendo esta movimentação, se preocupou e enviou várias tropas para a região que, mais tarde, foram derrotadas pela população. No meio tempo, José Maria foi assassinado, mas seus pensamentos continuaram em evidência e a guerra permaneceu. Após, o governo comprou a luta de forma grosseira e a tornou mais sangrenta (armamentos pesados e aviões de artilharia foram utilizados), matando os milhares de sertanejos e fazendo um acordo que estabelecesse os limites de cada região.</p>
             </section>
             </section>
             
@@ -58,15 +72,22 @@ function GuerraContestado() {
             <section className='blocoLado'>
                 <section className='blocoVideo'>
             <h2>Video</h2>
-            <iframe width="400" height="300" src="https://www.youtube.com/embed/bTOdnw2alHw?si=W2pUiBJTNOxwP87g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe className='video' width="400" height="300" src="https://www.youtube.com/embed/bTOdnw2alHw?si=W2pUiBJTNOxwP87g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             <h3>Descrição</h3>
-           <br/>
-            <hr/>
+           <p>é uma produção educativa do canal Débora Aladim, que apresenta de forma clara e objetiva os principais eventos e contextos históricos das duas mais emblemáticas revoltas populares do Brasil no período republicano: a Guerra de Canudos (1896–1897) e a Guerra do Contestado (1912–1916). 
+             O vídeo aborda: As causas sociais, políticas e econômicas que motivaram cada conflito; O papel das lideranças religiosas e messiânicas, como Antônio Conselheiro e José Maria; A repressão violenta do Estado brasileiro e o impacto dessas guerras na formação da identidade nacional.</p>
             </section>
             <section className='blocoVideo'>
             <h2 >Filme</h2>
-            <iframe width="400" height="300" src="https://www.youtube.com/embed/kbMUvt4NdtQ?si=Q3P53X9oI1u3rSDb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-            <h3>Descrição</h3>
+            <iframe className='video' width="400" height="300" src="https://www.youtube.com/embed/kbMUvt4NdtQ?si=Q3P53X9oI1u3rSDb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <h3>Sinopse</h3>
+            <p>A minissérie "A Guerra do Contestado", produzida pela RPC TV e exibida em 2012 no programa Casos e Causos, dramatiza um dos conflitos mais marcantes da história do Brasil: a Guerra do Contestado, ocorrida entre 1912 e 1916. Dividida em três episódios, a produção retrata a luta dos caboclos do interior de Santa Catarina e Paraná contra as forças do governo federal e estadual, em uma disputa por terras ricas em erva-mate e madeira .
+
+
+A narrativa é conduzida por Euzébio, um personagem fictício de passado misterioso, que transita pelos acontecimentos, revelando as emoções e desafios enfrentados pelos sertanejos durante o conflito . Com direção de Marcus Werneck, Beto Carminatti e André Siqueira, a minissérie destaca-se pela fidelidade histórica e pela qualidade de produção, contando com a participação da Orquestra Sinfônica do Paraná na trilha sonora .
+Tribuna do Paraná
+
+Ideal para estudantes, educadores e entusiastas da história brasileira, "A Guerra do Contestado" oferece uma visão envolvente e educativa sobre um episódio muitas vezes negligenciado nos livros didáticos.</p>
             </section>
             </section>
 
