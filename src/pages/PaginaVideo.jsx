0b6248f31df81ad6eb2b9d9e1e5df0bb1ca60779
Extreme <npyetro26@gmail.com>
2025-05-21 13:50:26 -0300

    site responsivo

--- src/pages/PaginaVideo.jsx
+++ src/pages/PaginaVideo.jsx
@@ -8,7 +8,7 @@ function PaginaVideo(){
      <h1 id='branco2'>VENHA ENTENDER MAIS SOBRE A <br/> <span id='roxo1'>  REVOLUÇÃO RUSSA </span></h1>
      <h1>REVOLUÇÃO RUSSA</h1>
      <section className="BlocoVideo">
-        <p>Video</p>
+     <iframe id="video2" width="560" height="315" src="https://www.youtube.com/embed/eERVu09GCWE?si=X1SRhNr9cMOyKIqT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
      </section>
 
 
