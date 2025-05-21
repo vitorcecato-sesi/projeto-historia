a26ad08f2149b55534a9c45472f030ce8b33a1b6
Extreme <npyetro26@gmail.com>
2025-05-21 12:12:22 -0300

    Cartaz

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import GuerraContestado from './pages/GuerraContestado.jsx'
 import RevolucaoRussa from './pages/RevolucaoRussa'
+import Cartaz from './pages/Cartaz.jsx'
 
 import GuerraCanudos from './pages/GuerraCanudos'
 import Home from './pages/Home'
@@ -9,7 +10,7 @@ function App() {
 
   return (
     <>
-   <RevolucaoRussa/>
+   <Cartaz/>
 
     </>
   )
--- src/pages/Cartaz.jsx
+++ src/pages/Cartaz.jsx
@@ -1,10 +1,20 @@
 import Navbar from "../components/Navbar";
+import './style/Cartaz.css'
 function Cartaz() {
     return (
         <div>
             <Navbar/>
+            <section className='Centro'>
+            <h1 id='branco1'>VENHA ENTENDER MAIS SOBRE O <br/> <span id='roxo'>  CARTAZ FEITO </span></h1>
         <h1>Cartaz</h1>
-        <p>Conteúdo do Cartaz</p>
+        </section>
+
+
+        <section className='Lado'>
+
+
+
+        </section>
         </div>
     );
 }
