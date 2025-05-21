d17aa958b57a5b2c098f4c4b87f718a045339f7b
Laura Betti <bettilaura0@gamail.com>
2025-05-21 14:18:47 -0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,6 @@ import RevolucaoDe1930 from './pages/RevolucaoDe1930'
 import SobreNos from './pages/SobreNos'
 import Home from "./pages/Home"
 import './App.css'
-import GuerraContestado from './pages/GuerraContestado.jsx'
-import RevolucaoRussa from './pages/RevolucaoRussa'
-
-import GuerraCanudos from './pages/GuerraCanudos'
-import Home from './pages/Home'
 
 function App() {
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,3 @@
-
-
 import Navbar from "../components/Navbar"
 import LogoSiteRoxo from "../assets/Logos/LogoSiteRoxo.png"
 import { Link } from "react-router-dom"
