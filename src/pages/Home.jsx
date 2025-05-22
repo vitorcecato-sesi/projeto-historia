5d72394413bbba1cd07640d83c933536e2411510
Lucas <lucascasagrandesesi@gmail.com>
2025-05-21 23:20:53 -0300

    Inserção de botão tema e footer na home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,20 @@ import "./style/Home.css"
 import Navbar from "../components/Navbar"
 import LogoSiteRoxo from "../assets/Logos/LogoSiteRoxo.png"
 import { Link } from "react-router-dom"
+import Footer from "../components/Footer"
+import BotaoTema from "../components/BotaoTema"
 
 function Home() {
     return (
         <>
             <Navbar logo={LogoSiteRoxo} />
             <section className="parallax1">
+                <section className="botTemaPosi">
+                <section className="botTema">
+             <BotaoTema/>
+             </section>
                 <h1 className="tituloParallax">CONHEÇA MAIS SOBRE OS <span className="corTexto">PRINCIPAIS EVENTOS HISTÓRICOS</span> ENTRE 1896-1930</h1>
+                </section>
                 <p className="textoParallax">
                     Entre 1896 e 1930, o Brasil e o mundo passaram por grandes transformações.
                     No país, conflitos como a Guerra de Canudos e a Guerra do Contestado mostraram a luta de comunidades contra a força do Estado.
@@ -32,31 +39,31 @@ function Home() {
                     </p>
             </section>
 
-            <h1 className="titulosHome">CONTEÚDOS:</h1>
+            <h1 id="conteudos" className="titulosHome">CONTEÚDOS:</h1>
 
                     <nav className="navHome">
-                        <Link to="/1">
+                        <Link to="/PrimeiraGuerraMundial">
                         <section className="navInfos">
                         <img className="navImg" src="https://upload.wikimedia.org/wikipedia/commons/9/98/Pris%C3%A3o_de_jagun%C3%A7os_pela_cavalaria.jpg" />
                         <p>Guerra de Canudos</p>
                         </section>
                         </Link>
 
-                        <Link to="/2">
+                        <Link to="/GuerraDoContestado">
                         <section className="navInfos">
                         <img className="navImg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyBVlWEP-XnVV_NfwNHLt7OaI4TtjSBH7ZmA&s" />
                         <p>Guerra do Contestado</p>
                         </section>
                         </Link>
 
-                        <Link to="/3">
+                        <Link to="/PrimeiraGuerraMundial">
                         <section className="navInfos"> 
                         <img className="navImg" src="https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/wwi-trench-archaeology-01.webp?w=1600&h=900" />
                         <p>Primeira Guerra Mundial</p>
                         </section>
                         </Link>
                         
-                        <Link to="/4">
+                        <Link to="/RevolucaoRussa">
                         <section className="navInfos">
                         <img className="navImg" src="https://assets.brasildefato.com.br/2024/09/image_processing20200201-29235-1nfnxod.jpg" />
                         <p>Revolução Russa</p>
@@ -64,21 +71,21 @@ function Home() {
                         </Link>
 
       
-                        <Link to="/5">
+                        <Link to="/FascismoItaliano">
                         <section className="navInfos">
                         <img className="navImg" src="https://www.politize.com.br/wp-content/uploads/2024/07/fascismo-entenda-o-conceito.jpg" />
                         <p>Fascismo Italiano</p>
                         </section>
                         </Link>
 
-                        <Link to="/6">
+                        <Link to="/CriseDe1929">
                         <section className="navInfos">
                         <img className="navImg" src="https://economianostra.wordpress.com/wp-content/uploads/2013/11/crise-anos-20.jpg" />
                         <p>Crise de 1929</p>
                         </section>
                         </Link>
     
-                        <Link to="/7">
+                        <Link to="/RevolucaoDe1930">
                         <section className="navInfos">
                         <img className="navImg" src="https://s1.static.brasilescola.uol.com.br/be/conteudo/images/getulio-vargas-no-centro-imagem-com-uniforme-militar-em-1930-5821dee50a0c0.jpg" />
                         <p>Revolução de 1930</p>
@@ -87,7 +94,7 @@ function Home() {
             
                 </nav>
 
-            <h1 className="titulosHome">PRODUÇÕES:</h1>
+            <h1 id="producoes" className="titulosHome">PRODUÇÕES:</h1>
 
             <nav className="navHome">
             
@@ -105,6 +112,7 @@ function Home() {
                         </section>
                         </Link>
             </nav>
+            <Footer corHeaderFooter="roxoClaro" corInfoFooter="roxoClaro" logo={LogoSiteRoxo}/>
         </>
     )
 }
