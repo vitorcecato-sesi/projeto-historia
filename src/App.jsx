2a54333673ad5dc5d1012ac5c72b80d38564a2e7
Lucas <lucascasagrandesesi@gmail.com>
2025-05-21 22:42:44 -0300

    Correção de bugs, melhora no nav(home)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import Home from './pages/Home'
 import PrimeiraGuerraMundial from './pages/Primeira-Guerra-Mundial'
 
