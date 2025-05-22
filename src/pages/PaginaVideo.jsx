713f55e49aebbf15164c8b6f0af1be67e508df96
Pietro <mellepietro3@gmail.com>
2025-05-22 01:35:20 -0300

    arrumando pagina home

--- src/pages/PaginaVideo.jsx
+++ src/pages/PaginaVideo.jsx
@@ -1,38 +1,44 @@
 import Navbar from "../components/Navbar";
-import './style/PaginaVideo.css'
-function PaginaVideo(){
-    return (
-     <>
-     <Navbar/>
-     <section className="Centro1">
-     <h1 id='branco2'>VENHA ENTENDER MAIS SOBRE A <br/> <span id='roxo1'>  REVOLUÇÃO RUSSA </span></h1>
-     <h1>REVOLUÇÃO RUSSA</h1>
-     <section className="BlocoVideo">
-     <iframe id="video2" width="560" height="315" src="https://www.youtube.com/embed/eERVu09GCWE?si=X1SRhNr9cMOyKIqT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-     </section>
+import "./style/PaginaVideo.css";
 
+import LogoSiteRoxo from "../assets/Logos/LogoSiteRoxo.png"
+import Footer from "../components/Footer"
+import BotaoTema from "../components/BotaoTema"
 
+function PaginaVideo() {
+  return (
+    <>
+      <Navbar logo={LogoSiteRoxo} />
 
+      <section className="Centro1">
 
+        <h1 id="branco2">
+          VENHA ENTENDER MAIS SOBRE A <br />
+          <span id="roxo1"> REVOLUÇÃO RUSSA </span>
+        </h1>
 
+        <BotaoTema/>
 
+        <section className="BlocoVideo">
 
+          <iframe
+            id="video2"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/eERVu09GCWE?si=X1SRhNr9cMOyKIqT"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
 
+        </section>
 
-     </section>
-     
-     
-     
-     
-     
-     
-     
-     
-     
+      </section>
 
-
-
-     </>
-    )
+      <Footer corHeaderFooter="roxoClaro" corInfoFooter="roxoClaro" logo={LogoSiteRoxo}/>
+    </>
+  );
 }
 export default PaginaVideo;
