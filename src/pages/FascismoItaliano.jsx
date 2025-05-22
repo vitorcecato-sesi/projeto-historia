fcb9c8c0f1f0edf2084feb5661d2e5829b0924ba
Lucas <lucascasagrandesesi@gmail.com>
2025-05-22 00:03:13 -0300

    Conclusão da page e preparação para a inserção do footer

--- src/pages/FascismoItaliano.jsx
+++ src/pages/FascismoItaliano.jsx
@@ -1,108 +1,124 @@
 import "./style/FascismoItaliano.css"
+import { useState } from "react";
 import Navbar from "../components/Navbar"
-import React, { useState } from 'react';
-import { Link } from "react-router-dom"
 import APIWikipedia from "../components/APIWikipedia";
 import BotaoCurtirTema from "../components/BotaoCurtirTema";
 import BotaoTema from "../components/BotaoTema"
+
 
 function FascismoItaliano() {
 
     const slides = [
         {
-            title: 'O que foi?',
-            description: 'Saiba o que foi o fascismo italiano.',
-            image: 'https://tribunadonorte.com.br/wp-content/uploads/2023/07/88353.jpg',
+            titulo: 'O que foi?',
+            descricao: 'Saiba o que foi o fascismo italiano.',
+            img: 'https://tribunadonorte.com.br/wp-content/uploads/2023/07/88353.jpg',
             id: '#0',
         },
         {
-            title: 'Contexto histórico',
-            description: 'Saiba o contexto histórico do fascismo italiano.',
-            image: 'https://static.historiadomundo.com.br/2024/09/benito-mussolini-lider-do-fascismo-italiano-entre-membros-dos-camisas-negras-especie-de-milicia-fascista.jpg',
+            titulo: 'Contexto histórico',
+            descricao: 'Saiba o contexto histórico do fascismo italiano.',
+            img: 'https://static.historiadomundo.com.br/2024/09/benito-mussolini-lider-do-fascismo-italiano-entre-membros-dos-camisas-negras-especie-de-milicia-fascista.jpg',
             id: '#1',
         },
         {
-            title: 'Governo Fascista',
-            description: 'Saiba sobre como foi o governo fascista.',
-            image: 'https://www.politize.com.br/wp-content/uploads/2024/07/fascismo-entenda-o-conceito.jpg',
+            titulo: 'Governo Fascista',
+            descricao: 'Saiba sobre como foi o governo fascista.',
+            img: 'https://www.politize.com.br/wp-content/uploads/2024/07/fascismo-entenda-o-conceito.jpg',
             id: '#2',
         },
         {
-            title: 'Fim do governo',
-            description: 'Saiba o que foi o fascismo italiano.',
-            image: 'https://medias.revistaoeste.com/wp-content/uploads/2022/08/fascismo-e-de-esquerda-2.jpg',
+            titulo: 'Fim do governo',
+            descricao: 'Saiba como foi o fim do governo fascista.',
+            img: 'https://medias.revistaoeste.com/wp-content/uploads/2022/08/fascismo-e-de-esquerda-2.jpg',
             id: '#3',
         },
         {
-            title: 'Características',
-            description: 'Saiba as características do governo fascista.',
-            image: 'https://static.mundoeducacao.uol.com.br/mundoeducacao/2020/02/benito-mussolini-me.jpg',
+            titulo: 'Características',
+            descricao: 'Saiba as características do governo fascista.',
+            img: 'https://static.mundoeducacao.uol.com.br/mundoeducacao/2020/02/benito-mussolini-me.jpg',
             id: '#4',
         },
         {
-            title: 'Benito Mussolini',
-            description: 'Saiba quem foi Benito Mussolini.',
-            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Benito_Mussolini_colored.jpg/500px-Benito_Mussolini_colored.jpg',
+            titulo: 'Benito Mussolini',
+            descricao: 'Saiba quem foi Benito Mussolini.',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Benito_Mussolini_colored.jpg/500px-Benito_Mussolini_colored.jpg',
             id: '#5',
         },
     ];
 
     // State for the current index
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const [indexAtual, setIndexAtual] = useState(0);
 
     // Function to navigate to the next slide
-    const nextSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+    const proxSlide = () => {
+        setIndexAtual((indexAnterior) => (indexAnterior + 1) % slides.length);
     };
 
     // Function to navigate to the previous slide
-    const prevSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
+    const anteSlide = () => {
+        setIndexAtual((indexAnterior) => (indexAnterior - 1 + slides.length) % slides.length);
     };
 
     // Get current, previous, and next slides
-    const prevIndex = (currentIndex - 1 + slides.length) % slides.length;
-    const nextIndex = (currentIndex + 1) % slides.length;
+    const anteIndex = (indexAtual - 1 + slides.length) % slides.length;
+    const proxIndex = (indexAtual + 1) % slides.length;
 
 
     return (
         <>
             <Navbar />
+
             <br></br>
-            <BotaoTema/>
+
+            <BotaoTema />
+
             <section className="carousel">
-                <button onClick={prevSlide} className="carousel-button">‹</button>
+
+                <button onClick={anteSlide} className="carousel-button">‹</button>
+
                 <section className="carousel-slide">
+
                     <section className="slideAP">
-                        <img src={slides[prevIndex].image} alt={slides[prevIndex].title} />
-                        <h2>{slides[prevIndex].title}</h2>
-                        <a id="aAP" href={slides[prevIndex].id}>{slides[prevIndex].description}</a>
+                        <img src={slides[anteIndex].img} alt={slides[anteIndex].titulo} />
+                        <h2>{slides[anteIndex].titulo}</h2>
+                        <a id="aAP" href={slides[anteIndex].id}>{slides[anteIndex].descricao}</a>
                     </section>
+
                     <section className="slide">
-                        <a></a>
-                        <img src={slides[currentIndex].image} alt={slides[currentIndex].title} />
-                        <h2>{slides[currentIndex].title}</h2>
-                        <a className="a" href={slides[currentIndex].id}>{slides[currentIndex].description}</a>
+                        <img src={slides[indexAtual].img} alt={slides[indexAtual].titulo} />
+                        <h2>{slides[indexAtual].titulo}</h2>
+                        <a className="a" href={slides[indexAtual].id}>{slides[indexAtual].descricao}</a>
                     </section>
 
                     <section className="slideAP">
-                        <img src={slides[nextIndex].image} alt={slides[nextIndex].title} />
-                        <h2>{slides[nextIndex].title}</h2>
-                        <a id="aAP" href={slides[nextIndex].id}>{slides[nextIndex].description}</a>
+                        <img src={slides[proxIndex].img} alt={slides[proxIndex].titulo} />
+                        <h2>{slides[proxIndex].titulo}</h2>
+                        <a id="aAP" href={slides[proxIndex].id}>{slides[proxIndex].descricao}</a>
                     </section>
+
                 </section>
-                <button onClick={nextSlide} className="carousel-button">›</button>
+
+                <button onClick={proxSlide} className="carousel-button">›</button>
+
             </section>
+
             <section className="box-geral">
+
                 <h1 className="tituloF">CONHEÇA MAIS SOBRE O <span className="verdeTexto">FASCISMO</span> <span className="vermelhoTexto">ITALIANO...</span></h1>
+
                 <h2 className="titulowiki">INFORMAÇÕES DA WIKIPÉDIA</h2>
-                 <APIWikipedia titulo="Itália Fascista" campoWiki="wikiFa" imagemID="imgWiki" imagemClass="imgWiki"/>
+
+                <APIWikipedia titulo="Itália Fascista" campoWiki="wikiFa" imagemID="imgWiki" imagemClass="imgWiki" />
+
                 <section className="box-infow" id="wikiFa">
                 </section>
+
                 <br></br>
+
                 <section className="imgout">
-                 <img id="imgWiki" src="" alt="" />
-                <p>Imagem Wikipédia</p>
+                    <img id="imgWiki" src="" alt="" />
+                    <p>Imagem Wikipédia</p>
                 </section>
 
                 <section id="0" className="box-info">
@@ -111,30 +127,32 @@ function FascismoItaliano() {
                         <p> O fascismo foi um regimento político totalitário liderado por Benito Mussolini, o movimento surgiu na itália em 1910 e foi oficializado como partido político em 1922, governando a itália
                             até 1943. O fascismo dominou o país inteiro (educação, economia, religião e política).
                         </p>
-                         <BotaoCurtirTema idSection="0" tema="Fascismo Italiano"/>
+                        <BotaoCurtirTema idSection="0" tema="Fascismo Italiano" />
                     </section>
                     <img src="https://cartasabertas.com.br/wp-content/uploads/2022/09/capa-nazismo-bandeira.jpg" />
                 </section>
-                
+
 
                 <section id="1" className="box-info">
                     <section className="box-coluna">
                         <h2>CONTEXTO HISTÓRICO</h2>
-
                         <p>Após a primeira guerra mundial, a Europa estava em caos, com uma grande crise econômica. A população se encontrava bastante desanimada.
                             São nessas condições que surgem alguns políticos e ideólogos, com discursos relacionados a resolver problemas complexos e  falar o que o povo mais queria ouvir, portanto, esse líder facista aproveitou o momento para apresentar um discurso parecido com o citado, sendo parecido com "Levante-se, orgulhe-se! A nação precisa de você, nós somos superiores".
                             Vale ressaltar que a Itália individualmente falando, também se encontrava em uma crise econômica, com sua moeda desvalorizada, aumento da inflação, a taxa de desemprego aumentando, diversas revoltas, greves e invasões de terra.
                             Em meio a tudo isso, os partidos de esquerda ganharam força, fato esse que incomodou os latifundiários e burgueses.
                             Todos esses fatores "justificam" a força e popularidade do fascismo italiano na época.
                         </p>
-                         <BotaoCurtirTema idSection="1" tema="Fascismo Italiano"/>
+                        <BotaoCurtirTema idSection="1" tema="Fascismo Italiano" />
                     </section>
                 </section>
+
                 <br></br>
+
                 <section className="imgout">
                     <img src="https://static.historiadomundo.com.br/2024/09/benito-mussolini-lider-do-fascismo-italiano-entre-membros-dos-camisas-negras-especie-de-milicia-fascista.jpg" />
                     <p>Contexto histórico</p>
                 </section>
+
                 <section className="box-info">
                     <section id="2" className="box-coluna">
                         <h2>GOVERNO FASCISTA</h2>
@@ -147,10 +165,12 @@ function FascismoItaliano() {
                             Em 1930, o fascismo incorpora o discurso de expansão territorial, ao declarar guerra à Etiópia. O conflito tinha como objetivo exaltar a "raça italiana", seus princípios e virtudes. Igualmente é época quando Mussolini se aproxima de Adolf Hitler, tendo como resultado
                             a promulgação das leis antissemitas onde os judeus italianos perderam seus direitos civis.
                         </p>
-                        <BotaoCurtirTema idSection="2" tema="Fascismo Italiano"/>
+                        <BotaoCurtirTema idSection="2" tema="Fascismo Italiano" />
                     </section>
                 </section>
+
                 <br></br>
+
                 <section className="imgout">
                     <img src="https://www.politize.com.br/wp-content/uploads/2024/07/fascismo-entenda-o-conceito.jpg" />
                     <p>Governo Fascista</p>
@@ -162,7 +182,7 @@ function FascismoItaliano() {
                         <h2>FIM DO GOVERNO</h2>
                         <p>O governo de Mussolini chega ao seu final em 1943, em um momento de sérias derrotas da Itália durante a Segunda Guerra Mundial. Assustado, Mussolini é levado pelos alemães para o norte e quando tenta escapar para a Alemanha é capturado e morto pelos partisans.
                         </p>
-                        <BotaoCurtirTema idSection="3" tema="Fascismo Italiano"/>
+                        <BotaoCurtirTema idSection="3" tema="Fascismo Italiano" />
                     </section>
                 </section>
 
@@ -185,10 +205,12 @@ function FascismoItaliano() {
                                 <li>10. Controle total do Estado fascista sobre assuntos relacionados à economia, política e cultura.</li>
                             </ul>
                         </section>
-                          <BotaoCurtirTema idSection="4" tema="Fascismo Italiano"/>
+                        <BotaoCurtirTema idSection="4" tema="Fascismo Italiano" />
                     </section>
                 </section>
+
                 <br></br>
+
                 <section className="imgout">
                     <img src="https://static.mundoeducacao.uol.com.br/mundoeducacao/2020/02/benito-mussolini-me.jpg" />
                     <p>Característiscas</p>
@@ -202,12 +224,14 @@ function FascismoItaliano() {
                             Nascido em 29 de julho de 1883, em uma cidade pequena localizada no sul da Itália, filho de pai ferreiro e mãe professora, cresceu com as histórias de nacionalismo e heroísmo político de seu pai socialista. Ainda jovem mudou-se para a Suíça e tornou-se um socialista
                             declarado, quando voltou para Itália, se estabeleceu como um jornalista socialista, porém futuramente Mussolini mudou sua ideologia política e criou um partido de extrema direita, em 1919.
                         </p>
-                        <BotaoCurtirTema idSection="5" tema="Fascismo Italiano"/>
+                        <BotaoCurtirTema idSection="5" tema="Fascismo Italiano" />
                     </section>
                 </section>
 
                 <section className="box-filmes">
+
                     <h1>HORA DO FILME...</h1>
+
                     <section className="box-videos">
                         <iframe width="580" height="350" src="https://www.youtube.com/embed/1crIvDB7Ios" title="Marcha sobre Roma e o fascismo italiano | Nerdologia" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                         <section className="box-coluna">
@@ -215,6 +239,7 @@ function FascismoItaliano() {
                             <p>O vídeo "Marcha sobre Roma e o fascismo italiano" do canal Nerdologia, apresentado por Felipe Figueiredo, oferece uma análise aprofundada sobre a ascensão do fascismo na Itália e o papel crucial da Marcha sobre Roma em 1922 na consolidação de Benito Mussolini no poder.</p>
                         </section>
                     </section>
+
                     <section className="box-videos">
                         <iframe width="580" height="350" src="https://www.youtube.com/embed/BPw5fxTPaIs" title="Filme - A Onda / Die Welle (2008) - Dublado" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                         <section className="box-coluna">
@@ -223,6 +248,7 @@ function FascismoItaliano() {
                         </section>
                     </section>
                 </section>
+
             </section>
         </>
     )
